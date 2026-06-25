53c5d31a3382133c59f094e2ee4324852f77af43
Jeya Dharshini C <cjeyadharshini2509@gmail.com>
2026-06-25 12:55:56 +0530

    add createadmin command

---
+++ chat/management/commands/createadmin.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+import os
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        username = 'admin'
+        password = os.environ.get('DJANGO_ADMIN_PASSWORD', 'admin123')
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, 'admin@example.com', password)
+            self.stdout.write('Superuser created')
+        else:
+            self.stdout.write('Superuser already exists')
