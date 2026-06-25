8935ca307867ed01a1f94478da908908611e37e1
Jeya Dharshini C <cjeyadharshini2509@gmail.com>
2026-06-25 14:40:10 +0530

    Admin pannel

--- chat/management/commands/createadmin.py
+++ chat/management/commands/createadmin.py
@@ -7,6 +7,8 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         username = 'admin'
         password = os.environ.get('DJANGO_ADMIN_PASSWORD', 'admin123')
-        User.objects.filter(username=username).delete()
-        User.objects.create_superuser(username, 'admin@example.com', password)
-        self.stdout.write('Superuser created fresh')
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, 'admin@example.com', password)
+            self.stdout.write('Superuser created')
+        else:
+            self.stdout.write('Superuser already exists')
